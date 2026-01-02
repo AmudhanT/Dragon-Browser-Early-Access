a40e6ea8824614a4010bad5e52fe6fafcd533499
Amudhan T <76392026+AmudhanT@users.noreply.github.com>
2026-01-02 18:26:25 +0530

    Update Settings.tsx

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDragon } from '../DragonContext';
 import { BrowserViewMode } from '../types';
 
@@ -8,13 +7,15 @@ import { BrowserViewMode } from '../types';
  * Settings logic is now handled in pages/Settings.tsx with flattened routing.
  * This file is kept as a placeholder to prevent import errors during migration.
  */
-export const Settings: React.FC<any> = () => {
+const Settings: React.FC = () => {
   const { navigateTo } = useDragon();
-  
+
   // Auto-redirect if somehow mounted
-  React.useEffect(() => {
+  useEffect(() => {
     navigateTo(BrowserViewMode.SETTINGS);
-  }, []);
+  }, [navigateTo]);
 
   return null;
 };
+
+export default Settings;
